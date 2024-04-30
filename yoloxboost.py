a3845ea548b22d5313003da869c777520b1c9428
yohanesnuwara <51282928+yohanesnuwara@users.noreply.github.com>
2024-04-30 11:48:40 +0200

    Update yoloxboost.py

--- yoloxboost.py
+++ yoloxboost.py
@@ -2,5 +2,13 @@ def setup():
   import subprocess
   def pip_install(name):
     subprocess.call(['pip', 'install', name])
-  pip_install('azure-storage-blob')
   pip_install('ultralytics')  
+
+def equalize_histogram():
+  # Pseudocode for equalization
+
+def stack(model_path_list):
+  # Pseudocode
+  for path in model_path_list:
+    model = YOLO(path)
+    # Predict model
